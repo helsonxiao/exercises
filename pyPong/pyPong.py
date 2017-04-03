214652a5dd66c68b21a7f7a24f037d5941def796
helsonxiao <helsonxiao@gmail.com>
2017-04-03 19:49:57 +0800

    second edition of pyPong

--- pyPong/pyPong.py
+++ pyPong/pyPong.py
@@ -1,4 +1,4 @@
-import pygame,sys
+import pygame,sys, random
 from pygame.locals import *
 
 
@@ -11,11 +11,14 @@ class MyBallClass(pygame.sprite.Sprite):
         self.speed = speed
 
     def move(self):
+        global score, score_font, score_surf
         self.rect = self.rect.move(self.speed)
         if self.rect.left <= 0 or self.rect.left > screen.get_width() :
             self.speed[0] = -self.speed[0]
         if self.rect.top <= 0 :
             self.speed[1] = -self.speed[1]
+            score = score + 1
+            score_surf = score_font.render(str(score), 1, (0, 0, 0))
 
 
 class MyPaddleClass(pygame.sprite.Sprite):
@@ -30,11 +33,16 @@ class MyPaddleClass(pygame.sprite.Sprite):
 pygame.init()
 screen = pygame.display.set_mode([640, 480])
 clock = pygame.time.Clock()
-ball_speed = [10, 5]
+ball_speed = [10, 10]
 myBall = MyBallClass("wackyball.bmp", ball_speed, [50,50])
 ballGroup = pygame.sprite.Group(myBall)
 paddle = MyPaddleClass([270,400])
-
+score = 0
+score_font = pygame.font.Font(None, 50)
+score_surf = score_font.render(str(score), 1, (0, 0 ,0))
+score_pos = [10, 10]
+lives = 3
+done = False
 running = True
 while running:
     clock.tick(30)
@@ -46,9 +54,33 @@ while running:
             paddle.rect.centerx = event.pos[0]
 
     if pygame.sprite.spritecollide(paddle, ballGroup, False):
-        myBall.speed[1] = -myBall.speed[1]
+        myBall.speed[1] = -myBall.speed[1] - random.random()
+
     myBall.move()
-    screen.blit(myBall.image, myBall.rect)
-    screen.blit(paddle.image, paddle.rect)
-    pygame.display.flip()
+
+    if not done:
+        screen.blit(myBall.image, myBall.rect)
+        screen.blit(paddle.image, paddle.rect)
+        screen.blit(score_surf, score_pos)
+        for i in range(lives):
+            width = screen.get_width()
+            screen.blit(myBall.image, [width - 40 * i, 20])
+        pygame.display.flip()
+
+    if myBall.rect.top >= screen.get_rect().bottom:
+        lives = lives - 1
+        if lives == 0:
+            final_text1 = "Game Over"
+            final_text2 = "Your final score is : " + str(score)
+            ft1_font = pygame.font.Font(None , 50)
+            ft1_surf = ft1_font.render(final_text1, 1, (0,0,0))
+            ft2_font = pygame.font.Font(None, 50)
+            ft2_surf = ft2_font.render(final_text2, 1, (0,0,0))
+            screen.blit(ft1_surf, [screen.get_width()/2 - ft1_surf.get_width()/2, 100])
+            screen.blit(ft2_surf, [screen.get_width()/ 2 - ft2_surf.get_width() / 2, 200])
+            pygame.display.flip()
+            done = True
+        else:  # wait 2 seconds, then start the next ball
+            pygame.time.delay(2000)
+            myBall.rect.topleft = [(screen.get_rect().width) - 40 * lives, 20]
 pygame.quit()
