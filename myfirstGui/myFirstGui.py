57057ee2b9043b929ff8f46a12141a51665fa9d2
helsonxiao <helsonxiao@gmail.com>
2017-04-12 20:27:02 +0800

    my first GUI

--- myfirstGui/myFirstGui.py
+++ myfirstGui/myFirstGui.py
@@ -8,6 +8,14 @@ class myWindowClass(QtGui.QMainWindow, form_class):
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self, parent)
         self.setupUi(self)
+        self.pushButton.clicked.connect(self.button_clicked)
+
+    def button_clicked(self):
+        x = self.pushButton.x()
+        y = self.pushButton.y()
+        x += 50
+        y += 50
+        self.pushButton.move(x,y)
 
 app = QtGui.QApplication(sys.argv)
 myWindow = myWindowClass()
