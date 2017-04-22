b9ef2616cd6621764e96af6d7ffe8d718b521076
helsonxiao <helsonxiao@gmail.com>
2017-04-22 10:09:30 +0800

    card game todo

--- Crazy_Eights/game.py
+++ Crazy_Eights/game.py
@@ -1,3 +1,6 @@
+#! python2
+# coding:utf-8
+
 from cards import Card
 import random
 
@@ -67,24 +70,64 @@ for i in range(5):
     computer.append(card)
     cards.remove(card)
 
-
 display = random.choice(cards)
+cards.remove(display)
 print('The display card is ' + display.long_name)
 print('...')
 
-for card in hand:
-    if card.suit == display.suit or card.rank == display.rank:
-        show_card(hand)
-        print('You have ' + card.short_name)
-        decision = raw_input('Play or not? Please enter Y/N: ')
-        judge(decision)
-    elif card.rank == 8:
-        show_card(hand)
-        print('You have a magic 8!')
-        decision = raw_input('Play or not? Please enter Y/N: ')
-        judge(decision)
-        suit_id = new_suit_id()
-    else:
-        print('Bad luck, no match. Card appended.')
-        append_card(hand)
+score = 0 # 玩家积分
+c_score = 0 # 电脑积分
 
+done = False
+while not done:
+    weapons = []    # 玩家的武器
+    for card in hand:
+        if card.suit == display.suit or \
+                        card.rank == display.rank or card.rank == 8:
+            # print('You have ' + card.short_name)
+            weapons.append(card)
+            # decision = raw_input('Play or not? Please enter Y/N: ')
+            # judge(decision)
+        # 这里存在一个逻辑性的 BUG
+        # elif card.rank == 8:
+        #     show_card(hand)
+        #     print('You have magic 8!')
+        #     decision = raw_input('Play or not? Please enter Y/N: ')
+        #     judge(decision)
+        #     suit_id = new_suit_id()
+    if not weapons:
+        print('Bad luck, no match was found.'
+              'Append a card to you.')
+        append_card(hand)
+    elif weapons:   # 出招吧！ todo
+        pass        # 玩家若出8，可以指定新的花色。若出对应牌则重新抽取展示牌。
+
+    if not hand:    # 玩家手牌消耗完毕，根据电脑手牌记分，然后重新发牌 todo
+        pass
+
+    c_weapons = []  # 电脑的武器
+    for card in computer:
+        if card.suit == display.suit or \
+                        card.rank == display.rank or card.rank == 8:
+            c_weapons.append(card)
+    if not c_weapons:
+        print("Good luck! Computer also doesn't have match card."
+              "Append a card to computer")
+        append_card(hand)
+    elif c_weapons: # 电脑随机出牌
+        c_choice = random.choice(c_weapons)
+        print('Computer shows its %s' % c_choice.short_name)
+        c_weapons.remove(c_choice)
+        if c_choice.value == 8: # 若电脑出8，随机改变花色。若出对应牌则重新选取展示牌。
+            c_suit = random.choice(['Diamonds', 'Hearts', 'Spades', 'Clubs'])
+            print('Computer chose a new suit - %s' % c_suit)
+            # todo
+
+    if not computer:    # 电脑手牌消耗完毕，根据玩家手牌记分，然后重新发牌 todo
+        pass
+
+    if not cards:   # 卡牌消耗完毕，根绝双方的手牌分别记分，重新发牌 todo
+        pass
+
+    if score >= 200 or c_score >= 200: # 率先达到200分的人取得胜利 todo
+        done = True
